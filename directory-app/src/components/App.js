847a757d4f397b96efdb0d2cb6bdf54dac67538b
abhi893 <abhishek98.ac@gmail.com>
2022-07-16 19:03:58 +0530

    working on the refreshing the contact list from localstorage

--- directory-app/src/components/App.js
+++ directory-app/src/components/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts"
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  }
+
+  useEffect(()=>{
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) {
+      console.log(retrieveContacts);
+      console.log("rerieving");
+    setContacts(retrieveContacts);
+    }
+  },[]);
+
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
   return (
     <div className='ui container'>
       <Header />
-      <AddContact />
-      <ContactList />
+      <AddContact addContactHandler = {addContactHandler}/>
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
--- directory-app/src/components/ContactList.js
+++ directory-app/src/components/ContactList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import CardContact from "./ContactCard";
 
-const ContactList=()=>{
+const ContactList=(props)=>{
+    //console.log(props);
+
+    const renderContactList = props.contacts.map((contact)=>{
+        return (
+           <CardContact contact={contact}></CardContact>
+        );
+    });
     return (
         <div className="ui celled list">
-            Contact List
+            {renderContactList}
         </div>
     )
 }
